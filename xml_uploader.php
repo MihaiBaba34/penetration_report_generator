e8f8b8982c4c50ceaad64fcdf98f1026dfa20971
Dorin Bujor <Dorin Bujor>
2016-11-05 15:00:49 +0200

    parsers

---
+++ xml_uploader.php
@@ -0,0 +1,45 @@
+<?php 
+
+echo "xml_uploader response";
+
+$target_dir = "uploads/";
+$uploadfile = $target_dir . basename($_FILES["userfile"]["name"]);
+$uploadOk = 0;
+$fileType = pathinfo($uploadfile,PATHINFO_EXTENSION);
+
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile))
+{	 
+	echo "File is uploaded successfully.\n";
+	$filename = $_FILES['userfile']['name'];
+	$uploadOk = 1;
+} 
+else 
+{
+	echo "File is not valid!\n";
+	$uploadOk = 0;
+}
+
+
+
+
+if($uploadOk == 1)
+{
+	echo "Success!";
+
+	$xmlDoc = simplexml_load_file($uploadfile);
+	if(!$xmlDoc) {		
+		die('Upload XML file.');
+	}
+	
+
+	
+
+}
+
+
+
+
+
+
+?>
